57671463770d37c31ad4efcbd1bb1056d9215053
purinliang <purinliang@gmail.com>
2026-08-10 02:07:20 +1000

    templates: 增加基础代码框架

---
+++ templates/base.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve() {}
+
+int main() {
+    int t = 1;
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
